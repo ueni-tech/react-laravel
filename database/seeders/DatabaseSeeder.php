7f0af8e928588028bf959b6e03e8f71f8e54cc4f
ueni.tech <ueni.tech@gmail.com>
2024-01-07 13:28:20 +0900

    DB作成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\ToDo;
+use App\Models\ToDoDetail;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,5 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $todo = ToDo::factory() -> create();
+        ToDoDetail::factory(5) -> create([
+            'to_do_id' => $todo -> id
+        ]);
     }
 }
