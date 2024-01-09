984133d0eea608c354a80afb2c06c91c728019ad
Kosuke Uenishi <uenishi_kosuke@ca-adv.co.jp>
2024-01-09 12:32:19 +0900

    home画面作成途中

--- resources/js/components/Main.js
+++ resources/js/components/Main.js
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import { Box } from "@mui/system";
+import Navigation from "./Navigation";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Example from "../pages/Example";
+import Home from "../pages/Home";
 
 function Main() {
-    return (
-        <Router>
-            <main className={"m-5"}>
-                <Switch>
-                    <Route path="/" exact component={Example} />
-                </Switch>
-            </main>
-        </Router>
-    );
+  return (
+    <Box>
+      <Navigation />
+      <Router>
+        <main className={"m-5"}>
+          <Switch>
+            <Route path="/" exact component={Home} />
+          </Switch>
+        </main>
+      </Router>
+    </Box>
+  );
 }
 
 export default Main;
---
+++ resources/js/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { Grid } from '@mui/material';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Home() {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={8}>
+        <div>Test</div>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Home;
